2e721e8044171e8f1d64b477577895497e06eccc
LightAntimag <antlighter@gmail.com>
2022-08-18 02:25:25 +0300

    Выполненны вступительные на курс Base Java. Героем этого ДЗ я объявляю метод findFirstEmptyIdx, который решает абсолютно все проблемы! Слава герою!

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,59 @@
-/**
- * Array based storage for Resumes
- */
+import java.util.Arrays;
+
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        Arrays.fill(storage, null);
     }
 
-    void save(Resume r) {
+    void save(Resume newResume) {
+        storage[findFirstEmptyIdx()] = newResume;
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < findFirstEmptyIdx(); i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                return storage[i];
+            }
+        }
+        System.out.println("Запрошенное резюме не найдено");
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < findFirstEmptyIdx() - 1; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                for (int j = i; j <= findFirstEmptyIdx(); j++) {
+                    if ((j + 1) > storage.length) {
+                        storage[j] = null;
+                        break;
+                    }
+                    storage[j] = storage[j + 1];
+                }
+            }
+        }
+
     }
 
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, findFirstEmptyIdx() >= 1 ? findFirstEmptyIdx() - 1 : findFirstEmptyIdx());
     }
 
     int size() {
-        return 0;
+        return findFirstEmptyIdx();
+    }
+
+    private int findFirstEmptyIdx() {
+        int count = 0;
+        for (Resume resume : storage) {
+            if (resume == null) {
+                return count;
+            } else {
+                count++;
+            }
+        }
+        // Свободного индекса нет весь массив заполнен, вот такой вот пока сигнал к этому.
+        return storage.length + 1;
     }
 }
