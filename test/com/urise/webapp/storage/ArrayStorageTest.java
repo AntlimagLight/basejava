e6766d20ab9c91f8f599f7cd57a3b724b19e3b00
LightAntimag <antlighter@gmail.com>
2022-09-03 08:50:11 +0300

    Внес небольшое изменение в поле Storage, про которое забыл вначале.

--- test/com/urise/webapp/storage/ArrayStorageTest.java
+++ test/com/urise/webapp/storage/ArrayStorageTest.java
@@ -1,6 +1,7 @@
 package com.urise.webapp.storage;
 
 public class ArrayStorageTest extends AbstractArrayStorageTest {
+    private Storage storage = new ArrayStorage();
 
     public ArrayStorageTest() {
         super(new ArrayStorage());
--- test/com/urise/webapp/storage/SortedArrayStorageTest.java
+++ test/com/urise/webapp/storage/SortedArrayStorageTest.java
@@ -1,6 +1,7 @@
 package com.urise.webapp.storage;
 
 public class SortedArrayStorageTest extends AbstractArrayStorageTest {
+    private Storage storage = new SortedArrayStorage();
 
     public SortedArrayStorageTest() {
         super(new SortedArrayStorage());
