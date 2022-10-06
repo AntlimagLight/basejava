f380d1821c29f4de1495b16ab0a8067cb0086a72
LightAntimag <antlighter@gmail.com>
2022-10-07 00:24:01 +0300

    minValue corrected
    after review

--- src/com/urise/webapp/lessons/MainLesson12.java
+++ src/com/urise/webapp/lessons/MainLesson12.java
@@ -8,7 +8,7 @@ import java.util.stream.IntStream;
 public class MainLesson12 {
     public static void main(String[] args) {
 
-        int[] arr = {3,5,5,4,8,8,2};
+        int[] arr = {9,5,5,4,8,8,2};
         System.out.println("___");
         System.out.println(minValue(arr));
 
@@ -23,20 +23,11 @@ public class MainLesson12 {
         if (IntStream.of(values).anyMatch(n -> n < 1 | n > 9)) {
             throw new NumberFormatException("Элементы массива должны быть цифрами от 1 до 9");
         }
-        int[] filteredValues = IntStream
+        return IntStream
                 .of(values)
                 .distinct()
-                .toArray();
-        int result = 0;
-        for (int i = filteredValues.length; i > 0 ; i--) {
-            int min = IntStream
-                    .of(filteredValues)
-                    .min()
-                    .getAsInt();
-            result += Math.pow(10, i - 1) * min;
-            filteredValues = IntStream.of(filteredValues).filter(n -> n > min).toArray();
-        }
-        return result;
+                .sorted()
+                .reduce(0, (a, b) -> a * 10 + b );
     }
 
     static List<Integer> oddOrEven(List<Integer> integers) {
